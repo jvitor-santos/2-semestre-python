4df72cfaff63b00a3ba2059b21912f3cb3aa4bef
jvitorxsantos <116177071+jvitorxsantos@users.noreply.github.com>
2022-10-19 16:40:58 -0300

    Initial commit

---
+++ linguagem de programacao/atividade 09-02-2021 while.py
@@ -0,0 +1,14 @@
+i=0
+soma=0
+con=0
+qtd=0
+while i<10:
+    n=int(input("Digite o numero"))
+    if n%2==0:
+        par=n
+        soma=soma+par
+    if n<10:
+        qtd+=1
+    i=i+1
+print("A soma dos numeros pares é de:",soma)
+print("A quantidade de numeros menores que 10 é de:",qtd)
---
+++ linguagem de programacao/atividade idade 09-02-2021.py
@@ -0,0 +1,9 @@
+idade=int(input("Digite sua idade: "))
+if idade<=12:
+    print("Você é criança")
+elif idade<=17:
+    print("Você é adolescente")
+elif idade<=59:
+    print("Você é adulto")
+elif idade>=60:
+    print("Você é idoso")
